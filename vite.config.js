8b588087f16f5f4e8afb3e19703bd903fd3a35fe
Hamzah SM <hamzahsaiful782@gmail.com>
2024-03-06 10:29:58 +0700

    crud laravel react tasklists

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+// });
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import 'dotenv/config';
+
+export default defineConfig({
+    build: {
+        minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
+        cssMinify: process.env.APP_ENV === 'production',
+    },
+    plugins: [
+        laravel({
+            input: ['resources/react/app.tsx'],
+            refresh: true,
+        }),
+        react(), 
+    ],
+});
